4a07797e16313f529672334bbf88f3740a7897c7
onelson <thebantonn@gmail.com>
2026-03-17 18:05:35 +0100

    intégration de la page login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,29 @@
+import React from 'react';
+
 function Login() {
-  return <h1>Page de Connexion</h1>
+  return (
+    <main className="main bg-dark">
+      <section className="sign-in-content">
+        <i className="fa fa-user-circle sign-in-icon"></i>
+        <h1>Sign In</h1>
+        <form>
+          <div className="input-wrapper">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
+          </div>
+          <div className="input-remember">
+            <input type="checkbox" id="remember-me" />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+          <button className="sign-in-button">Sign In</button>
+        </form>
+      </section>
+    </main>
+  )
 }
-export default Login
+
+export default Login;
--- frontend/src/pages/Profile.jsx
+++
@@ -1,4 +0,0 @@
-function Profile() {
-  return <h1>Page de Profil</h1>
-}
-export default Profile;
